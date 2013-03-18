ef932d91022a1e88b4df2a497df6eb593368c39c
zcaudate <z@caudate.me>
2013-03-18 18:18:01 +0800

    Gutted for refactoring

--- src/adi/json.clj
+++
@@ -1,8 +0,0 @@
-(ns adi.json
-  (:use adi.utils)
-  (:require [adi.data :as ad]
-            [adi.schema :as as]))
-
-
-(defn from-json-map [schm jm]
-	)
--- test/adi/data/test_08_unprocess_basic.clj
+++
@@ -1,6 +0,0 @@
-(ns adi.data.test-08-unprocess-basic
-  (:use midje.sweet
-        adi.utils
-        adi.checkers)
-  (:require [adi.data :as ad]))
-
