3dac438ef731c053b8b5c3b4bf9836ddf5654c3c
Chris Zheng <z@caudate.me>
2013-05-29 16:03:11 +1000

    version 0.1.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject adi "0.1.5-SNAPSHOT"
+(defproject adi "0.1.5"
   :description "adi (a datomic interface)"
   :url "http://www.github.com/zcaudate/adi"
   :license {:name "Eclipse Public License"
