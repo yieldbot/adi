3c21fa33fa60e53f3e66b3ddfc2efa45f346696e
Chris Zheng <z@caudate.me>
2014-10-30 05:09:09 +0800

    Exposed Documentation

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # adi 
 [![Build Status](https://travis-ci.org/zcaudate/adi.png?branch=master)](https://travis-ci.org/zcaudate/adi)
 
-Still in development. For information, see [earlier version](https://github.com/zcaudate/adi/tree/v1)
+Still in development. Documentation in progress [here](http://docs.caudate.me/adi)
+
+The [earlier version](https://github.com/zcaudate/adi/tree/v1) has been moved to the `v1` branch.
 
--- project.clj
+++ project.clj
@@ -21,8 +21,7 @@
 
   :profiles {:dev {:plugins [[lein-midje "3.1.1"]
                              [lein-midje-doc "0.0.24"]]
-                   :dependencies [;;[com.datomic/datomic-free "0.8.3971"]
-                                  [com.datomic/datomic-free "0.9.5052" :exclusions [joda-time]]
+                   :dependencies [[com.datomic/datomic-free "0.9.5052" :exclusions [joda-time]]
                                   [midje "1.6.3"]
                                   [clj-time "0.6.0"]
                                   [me.raynes/fs "1.4.5"]
