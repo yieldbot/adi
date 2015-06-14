0cddb0b0820121bddfaa7bba7709b1f068fc252d
Chris Zheng <z@caudate.me>
2015-06-14 13:15:28 +1000

    updating deps

--- project.clj
+++ project.clj
@@ -4,13 +4,13 @@
   :license {:name "The MIT License"
             :url "http://http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [im.chit/hara.common    "2.1.7"]
-                 [im.chit/hara.string    "2.1.7"]
-                 [im.chit/hara.data      "2.1.7"]
-                 [im.chit/hara.function  "2.1.7"]
-                 [im.chit/hara.component "2.1.7"]
-                 [im.chit/ribol "0.4.0"]
-                 [inflections "0.9.9"]]
+                 [im.chit/hara.common    "2.1.12"]
+                 [im.chit/hara.string    "2.1.12"]
+                 [im.chit/hara.data      "2.1.12"]
+                 [im.chit/hara.function  "2.1.12"]
+                 [im.chit/hara.component "2.1.12"]
+                 [im.chit/ribol "0.4.1"]
+                 [inflections "0.9.14"]]
 
   :documentation {:files {"docs/index"
                        {:input "test/midje_doc/adi_guide.clj"
