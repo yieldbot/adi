dbc07e8dcc6f45a58d14094901c568846dcd5a26
Chris Zheng <z@caudate.me>
2017-06-06 13:52:14 +1000

    fixing tests

--- test/spirit/rabbitmq/request_test.clj
+++ test/spirit/rabbitmq/request_test.clj
@@ -4,13 +4,13 @@
             [spirit.rabbitmq :as rabbitmq]
             [cheshire.core :as json]))
 
-^{:refer spirit.rabbitmq.http.request/create-url :added "0.5"}
+^{:refer spirit.rabbitmq.request/create-url :added "0.5"}
 (fact "creates the management url"
 
   (create-url rabbitmq/*default-options* "hello")
   => "http://localhost:15672/api/hello")
 
-^{:refer spirit.rabbitmq.http.request/wrap-parse-json :added "0.5"}
+^{:refer spirit.rabbitmq.request/wrap-parse-json :added "0.5"}
 (fact "returns the body as a clojure map"
 
   ((wrap-parse-json identity)
@@ -18,14 +18,14 @@
     :body (json/generate-string {:a 1 :b 2})})
   => {:a 1, :b 2})
 
-^{:refer spirit.rabbitmq.http.request/update-nested-keys :added "0.5"}
+^{:refer spirit.rabbitmq.request/update-nested-keys :added "0.5"}
 (fact "updates keys in the nesting"
   
   (update-nested-keys {:a {:b {:c 1}}}
                       #(keyword (str (name %) "-boo")))
   => {:a-boo {:b-boo {:c-boo 1}}})
 
-^{:refer spirit.rabbitmq.http.request/wrap-generate-json :added "0.5"}
+^{:refer spirit.rabbitmq.request/wrap-generate-json :added "0.5"}
 (fact "returns the body as a json string"
   
   ((wrap-generate-json identity)
@@ -33,7 +33,7 @@
     :body {:a 1 :b 2}})
   => {:status 200, :body "{\"a\":1,\"b\":2}"})
 
-^{:refer spirit.rabbitmq.http.request/request :added "0.5"}
+^{:refer spirit.rabbitmq.request/request :added "0.5"}
 (fact "creates request for the rabbitmq management api"
 
   (request rabbitmq/*default-options* "cluster-name")
