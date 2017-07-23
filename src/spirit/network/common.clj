20b58daef8672dee48d58ae9daea5657ad912529
Chris Zheng <z@caudate.me>
2017-07-23 22:15:15 +0930

    created common framework

--- src/spirit/network/common.clj
+++ src/spirit/network/common.clj
@@ -1,4 +1,5 @@
-(ns spirit.network.common)
+(ns spirit.network.common
+  (:require [clojure.core.async :as async]))
 
 (defmulti read-value  (fn [string format] format))
 
@@ -13,22 +14,114 @@
   (pr-str data))
 
 (defn pack [{:keys [format]}
-            {:keys [id type status params data] :as package}]
+<<<<<<< Local Changes
+            {:keys [type delivery params] :as package}]
+=======
+            package]
+>>>>>>> External Changes
   (try
     (write-value package format)
     (catch Exception e
-      (write-value {:id id
-                    :type   :error
-                    :input  {:type type
-                             :data data
-                             :params params}
-                    :data   {:message (ex-info e)}} format))))
+      (write-value {:type :error/write-value
+                    :code :error
+                    :error   {:message (ex-data e)}} format))))
 
 (defn unpack [{:keys [format]} message]
   (try
     (read-value message format)
     (catch Exception e
-      {:id     :error/tansport
-       :type   :error
-       :input  {:string message}
-       :data   {:message (ex-info e)}} format)))
+      {:type :error/read-value
+       :code :error
+       :input   message
+       :error   {:message (ex-data e)}} format)))
+
+(defn random-uuid []
+  (str (java.util.UUID/randomUUID)))
+
+(defn wrap-request [handler]
+  (fn [{:keys [pending return] :as conn}
+       {:keys [tag] :as package}]
+    (let [tag    (or tag (random-uuid))
+          ch     (async/chan)
+          _      (if-let [timeout (:timeout return)]
+                   (async/go (async/timeout timeout)
+                             (println "TIMEOUT")
+                             (async/put! ch
+                                         {:type :error/timeout
+                                          :code :error
+                                          :tag tag
+                                          :error {:message (str "timeout for " tag)}})
+                             (async/close! ch)
+                             (swap! pending dissoc tag)))
+          _      (swap! pending assoc tag ch)
+          result (handler conn (assoc package :code :request :tag tag))]
+      (case (:type return)
+        :channel ch
+        :value   (async/<!! ch)))))
+
+(defn wrap-unpack [handler]
+  (fn [conn message]
+    (let [package (unpack conn message)]
+      (handler conn package))))
+
+(defn dead-fn
+  [conn package]
+  (println "DEAD PACKAGE:" package))
+
+(defn process-fn
+  [{:keys [handlers] :as conn}
+   {:keys [type] :as package}]
+  ((get handlers type) package))
+
+(defn return-fn
+  [{:keys [pending fn] :as conn}
+   {:keys [tag response] :as package}]
+  (if-let [ch (get @pending tag)]
+    (do (async/put! ch package)
+        (async/close! ch)
+        (swap! pending dissoc tag))
+    ((:dead fn) conn package)))
+
+(defn receive-fn [{:keys [fn] :as conn} {:keys [code] :as package}]
+  (case code
+    :request  ((:respond fn) conn package)
+    :response ((:return fn) conn package)
+    ((:process fn) conn package)))
+
+(defn wrap-response [handler]
+  (fn [{:keys [fn] :as conn} package]
+    (let [result (handler conn package)]
+      ((:send fn) conn (merge package {:code :response
+                                       :response result})))))
+
+(defn init-functions [{:keys [format fn] :as conn}]
+  (let [pending    (atom {})
+        send-fn    (or (:send fn)
+                       (throw (ex-info "send function is required" {})))
+        attach-fn  (or (:attach fn)
+                       (throw (ex-info "attach function is required" {})))
+        
+        request-fn (-> send-fn
+                       (wrap-request))
+        
+        process-fn (or (:process fn) process-fn)
+        respond-fn (-> process-fn
+                       (wrap-response))
+        
+        return-fn  (or (:return fn) return-fn)
+        receive-fn (-> (or (:receive fn) receive-fn)
+                       (wrap-unpack))
+
+        dead-fn    (or (:dead fn) dead-fn)
+        conn       (assoc conn
+                          :pending pending
+                          :fn {:attach  attach-fn
+                               :dead    dead-fn
+                               :process process-fn
+                               :receive receive-fn
+                               :request request-fn
+                               :respond respond-fn
+                               :return  return-fn
+                               :send    send-fn})
+        _ (attach-fn conn receive-fn)]
+    conn))
--- test/spirit/network/singleton_test.clj
+++ test/spirit/network/singleton_test.clj
@@ -1,21 +1,35 @@
 (ns spirit.network.singleton-test
   (:use hara.test)
   (:require [spirit.network.singleton :refer :all]
-            [hara.component :as component]))
+            [hara.component :as component]
+            [clojure.core.async :as async]))
 
 (fact "test for send and receive via transport"
 
   (def network (singleton {:format :edn
-                           :handler {:on/id (fn [req]
-                                              (prn req)
-                                              :on/id)}}))
+                           :options {:network {:delay 100}}
+                           :return {:type :channel
+                                    :timeout 100}
+                           :handlers {:on/id (fn [req]
+                                               (prn req)
+                                               :on/id)}}))
   
-  (:fn network)
+  ((-> network :fn :send) network {:type :on/id})
+  (def ch ((-> network :fn :request) network {:type :on/id}))
+  (time (async/<!! ((-> network :fn :request) network {:type :on/id})))
   
-  
-  ((-> network :fn :send) network {:id :on/id})
-  
-  @(:raw network)
-  
-  (component/stop network)
+  (:raw network)
+  (:pending network)
+
+  (comment
+    (async/<!!
+     (async/go (async/<! (async/alt! (async/go
+                                       (async/timeout 1000)
+                                       "hello")
+                                     
+                                     (async/chan)))))
+    
+    
+    (async/<!! (async/timeout 1000))
+    (component/stop network))
   )
